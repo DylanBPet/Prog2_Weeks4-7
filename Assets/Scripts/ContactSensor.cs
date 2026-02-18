afe68bc9fc604bd32c130888f4d6891426bf8bfb
petrofdy <dylanpetroff13@gmail.com>
2026-02-18 10:49:00 -0500

    Used Unity Event!

---
+++ Assets/Scripts/ContactSensor.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ContactSensor : MonoBehaviour
+{
+    public SpriteRenderer hazard;
+    public bool isInHazard = false;
+
+    public UnityEvent OnEnterSensor;
+
+    public UnityEvent<float> OnRandomNumber;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (hazard.bounds.Contains(transform.position) == true)
+        {
+            if(isInHazard == true)
+            {
+                //still in the hazard
+            }
+            else
+            {
+                //you entered the hazard
+                isInHazard = true;
+                OnEnterSensor.Invoke();
+            }       
+        }
+        else
+        {
+            if (isInHazard == true)
+            {
+                //you were in hazard last frame, but not anymore
+                isInHazard = false;
+                OnRandomNumber.Invoke(Random.Range(0, 10));
+            }
+            else
+            {
+                //you are still outside the hazard
+            }
+        }
+
+
+    }
+
+    public void ShowNumber(float number)
+    {
+        Debug.Log(number);
+    }
+}
